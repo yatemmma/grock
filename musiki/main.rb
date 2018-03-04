f6722858b6d4048b7cb61c5f509d87124a9f25e7
yatemmma <yatemmma@gmail.com>
2018-03-05 02:28:19 +0900

    wip

--- musiki/main.rb
+++ musiki/main.rb
@@ -1,13 +1,22 @@
+require "erb"
+require "fileutils"
 require "./musiki/metadata"
 
 def read_files(dir)
   data = {}
   Dir.glob("src/#{dir}/*").each do |path|
-    text = File.open(path).read
+    text = File.read(path)
     banddata = Metadata.new(text)
     data[banddata[:code]] = banddata
   end
   data
+end
+
+def output_html(obj, template, file)
+  contents = File.read("musiki/templates/#{template}.erb")
+  path = "output/#{file}.html"
+  FileUtils.mkdir_p(File.dirname(path))
+  File.write(path, ERB.new(contents).result(binding))
 end
 
 @labels = read_files("label")
@@ -15,6 +24,10 @@ end
 @songs = read_files("song")
 @bands = read_files("band")
 @discs = read_files("disc")
+
+@labels.each do |code, label|
+  output_html(label, "label", "label/#{label[:code]}")
+end
 
 puts @labels
 puts @members

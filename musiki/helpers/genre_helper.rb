3111cc3d486ded0843fd26ec50ec399a0171af6a
yatemmma <yatemmma@gmail.com>
2018-03-11 23:54:38 +0900

    wip

--- musiki/helpers/genre_helper.rb
+++ musiki/helpers/genre_helper.rb
@@ -4,12 +4,18 @@ module GenreHelper
   }
 
   def genre_name(code)
-    result = GENRES[code]
+    result = GENRES[code.to_sym]
     raise "Genre name not found. code:#{code}" if result.nil?
     result
   end
-  
+
   def main_genre
-    genres.nil? ? "" : GENRES[genres.first]
+    if genres.nil?
+      ""
+    elsif genres.is_a? String
+      genre_name(genres)
+    else
+      genre_name(genres.first)
+    end
   end
 end

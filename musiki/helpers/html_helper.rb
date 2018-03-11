89115b39de81a2996fbc14d79c6e1c7ec07c66c4
yatemmma <yatemmma@gmail.com>
2018-03-11 19:08:22 +0900

    wip

--- musiki/helpers/html_helper.rb
+++ musiki/helpers/html_helper.rb
@@ -1,3 +1,12 @@
 module HtmlHelper
-
+  def headers(title, level = 0)
+    html = <<-"EOS"
+      <meta charset="utf-8">
+      <title>#{title}</title>
+      <meta name="viewport" content="width=device-width,initial-scale=1">
+      <link href="./#{"../"*level}assets/style.css" media="all" rel="stylesheet" />
+      <script src="./#{"../"*level}assets/script.js" async></script>
+    EOS
+    html
+  end
 end

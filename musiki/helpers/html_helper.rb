47ff2985ba305e623208ae9087b00f6bf628cdb6
yatemmma <yatemmma@gmail.com>
2018-03-11 20:02:31 +0900

    wip

--- musiki/helpers/html_helper.rb
+++ musiki/helpers/html_helper.rb
@@ -1,5 +1,5 @@
 module HtmlHelper
-  def headers(title, level = 0)
+  def head(title, level = 0)
     html = <<-"EOS"
       <meta charset="utf-8">
       <title>#{title}</title>
@@ -8,5 +8,21 @@ module HtmlHelper
       <script src="./#{"../"*level}assets/script.js" async></script>
     EOS
     html
+  end
+
+  def header(level = 0)
+    html = <<-"EOS"
+      <header>
+        <h1><a href="./#{"../"*level}index.html">G-ROCK</a></h1>
+        <ul>
+          <li><a href="./#{"../"*level}bands.html">Bands</a></li>
+          <li><a href="./#{"../"*level}discs.html">Discs</a></li>
+          <li><a href="./#{"../"*level}labels.html">Labels</a></li>
+          <li><a href="./#{"../"*level}songs.html">Songs</a></li>
+          <li><a href="./#{"../"*level}members.html">Members</a></li>
+        </ul>
+        <div><a href="#" onclick="openMenu()">MENU</a></div>
+      </header>
+    EOS
   end
 end

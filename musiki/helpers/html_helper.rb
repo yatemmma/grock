842846b944a4b826f2f7f456ee685eb84840d414
yatemmma <yatemmma@gmail.com>
2018-03-11 20:39:52 +0900

    wip

--- musiki/helpers/html_helper.rb
+++ musiki/helpers/html_helper.rb
@@ -5,9 +5,9 @@ module HtmlHelper
       <title>#{title}</title>
       <meta name="viewport" content="width=device-width,initial-scale=1">
       <link href="./#{"../"*level}assets/style.css" media="all" rel="stylesheet" />
+      <link href="./#{"../"*level}assets/vendor/css/fontawesome-all.min.css" media="all" rel="stylesheet" />
       <script src="./#{"../"*level}assets/script.js" async></script>
     EOS
-    html
   end
 
   def header(level = 0)
@@ -23,6 +23,17 @@ module HtmlHelper
         </ul>
         <div><a href="#" onclick="openMenu()">MENU</a></div>
       </header>
+    EOS
+  end
+
+  def footer(level = 0)
+    html = <<-"EOS"
+      <footer>
+        <div class="link">
+          <a href="https://twitter.com/prettygereen" target="_blank"><i class="fab fa-twitter"></i></a>
+        </div>
+        <div class="copy">&copy; 2009-#{Date.today.strftime("%Y")} <a href="./#{"../"*level}index.html">G-ROCK</a></div>
+      </footer>
     EOS
   end
 end

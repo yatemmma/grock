75267a5e61acb002e5ff7b3eb78db08b1ad0d39a
yatemmma <yatemmma@gmail.com>
2018-05-19 15:30:12 +0900

    wip

--- server/band_controller.rb
+++ server/band_controller.rb
@@ -1,9 +1,19 @@
 class App < Sinatra::Base
 
   get "/:types.html" do |types|
-    clazz = Object.const_get(types.singularize.capitalize)
-    @items = clazz.all_items
-    erb types.to_sym, {title: types.capitalize, search_data: clazz.search_json}
+    case types
+    when "bands"
+      @items = Band.all_items.select {|x| (x.member_of.empty? && x.ex_member_of.empty?)}
+      erb :bands, {title: "Bands", search_data: Band.search_json}
+    when "members"
+      @items = Band.all_items.reject {|x| (x.member_of.empty? || x.ex_member_of.empty?)}
+      erb :bands, {title: "Members", search_data: Band.search_json}
+    else
+      clazz = Object.const_get(types.singularize.capitalize)
+      @items = clazz.all_items
+      erb types.to_sym, {title: types.capitalize, search_data: clazz.search_json}
+    end
+
   end
 
   # get "/bands.html" do

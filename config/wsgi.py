173e2dbdef6a5c9dcf5e75d62c32d5fe6904d6b3
yatemmma <yatemmma@gmail.com>
2020-01-25 08:04:21 +0900

    wip

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from wsgi_basic_auth import BasicAuth
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_wsgi_application()
+DEBUG = getattr(settings, "DEBUG", None)
+
+if DEBUG:
+    application = get_wsgi_application()
+else:
+    application = BasicAuth(get_wsgi_application())

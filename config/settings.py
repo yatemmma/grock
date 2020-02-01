3761965fcaab209721bf22bdfd86a0c965f66e4b
yatemmma <yatemmma@gmail.com>
2020-02-01 23:38:11 +0900

    wip

--- config/settings.py
+++ config/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/assets/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'docs', 'assets')
+    os.path.join(BASE_DIR, 'docs', 'static')
 ]
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)

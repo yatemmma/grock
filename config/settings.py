41672b74958a39b9e30ccfef4cd980fd0adc1b35
yatemmma <yatemmma@gmail.com>
2020-02-22 14:46:59 +0900

    wip

--- config/settings.py
+++ config/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASS'],
-        'HOST': os.environ['DB_HOST'],
+        'NAME': 'name',
+        'USER': 'user',
+        'PASSWORD': '',
+        'HOST': 'host',
         'PORT': '5432',
     }
 }
